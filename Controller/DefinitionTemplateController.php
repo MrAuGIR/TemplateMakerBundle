e94ac5ab27d629f64dfdb2be653e2b607edd9e9a
agirard <augirard17@gmail.com>
2023-02-05 23:19:03 +0100

    add endpoint in controller

---
+++ Controller/DefinitionTemplateController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TemplateMakerBundle\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefinitionTemplateController extends \Pimcore\Controller\FrontendController
+{
+    #[Route("/template/list/template", name: "template_list_template", methods: ["GET"])]
+    public function getListTemplateDefinition(Request $request) : JsonResponse {
+
+        return $this->json([],200);
+    }
+
+    #[Route("/template/create", name: "template_create_definition", methods: ["POST"])]
+    public function createNewTemplateDefinition(Request $request) : JsonResponse {
+
+        return $this->json([],200);
+    }
+}
