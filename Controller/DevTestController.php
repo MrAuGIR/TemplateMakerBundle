6dd748109594edd70979e24b23623228e5d08021
agirard <augirard17@gmail.com>
2023-02-19 21:40:53 +0100

    update element model

--- Controller/DevTestController.php
+++ Controller/DevTestController.php
@@ -2,18 +2,26 @@
 
 namespace TemplateMakerBundle\Controller;
 
+use ActiveWireframeBundle\Model\Elements;
 use Pimcore\Controller\FrontendController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use TemplateMakerBundle\Model\DataObject\Element;
 
-class DefaultController extends FrontendController
+class DevTestController extends FrontendController
 {
     /**
      * @Route("/template_maker")
      */
     public function indexAction(Request $request)
     {
+
+        $element = Element::getById(1);
+
+        dd($element);
+
+
         return new Response('Hello world from template_maker');
     }
 }
--- Model/DataObject/Element.php
+++ Model/DataObject/Element.php
@@ -7,11 +7,17 @@ use Pimcore\Model\Exception\NotFoundException;
 
 class Element extends AbstractModel
 {
-    public int $id;
+    public ?int $id;
 
-    public array $positions;
+    public ?array $positions;
 
-    public string $styles;
+    public ?string $styles;
+
+    public ?string $filter;
+
+    public ?string $type;
+
+    public ?string $dataTransformer;
 
     /**
      * get score by id
@@ -31,30 +37,57 @@ class Element extends AbstractModel
         return null;
     }
 
-    public function getId() : int {
-        return $this->id;
+    public function getId() : ?int {
+        return $this->id ?? null;
     }
 
-    public function setId(int $id) : self {
+    public function setId(?int $id) : self {
         $this->id = $id;
         return $this;
     }
 
-    public function getPositions() : array {
+    public function getPositions() : ?array {
         return $this->positions;
     }
 
-    public function setPositions(array $position) : self {
+    public function setPositions(?array $position) : self {
         $this->positions = $position;
         return $this;
     }
 
-    public function getStyles() : string {
+    public function getStyles() : ?string {
         return $this->styles;
     }
 
-    public function setStyles(string $styles) : self {
+    public function setStyles(?string $styles) : self {
         $this->styles = $styles;
+        return $this;
+    }
+
+    public function getType() : ?string {
+        return $this->type;
+    }
+
+    public function setType(?string $type) : self {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function getFilter() : ?string {
+        return $this->filter;
+    }
+
+    public function setFiler(?string $filter) : self {
+        $this->filter = $filter;
+        return $this;
+    }
+
+    public function getDataTransformer() : ?string {
+        return $this->dataTransformer;
+    }
+
+    public function setDataTransformer(?string $transformer) : self {
+        $this->dataTransformer = $transformer;
         return $this;
     }
 }
